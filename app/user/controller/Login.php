bbc3d2730e882ee48cd38c86d86eaf0b25d8ef3f
siyizhen <1193814298@qq.com>
2018-02-26 23:07:28 +0800

    部分程序

--- app/user/controller/Login.php
+++ app/user/controller/Login.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\user\controller;
 use think\Controller;
+use clt\Leftnav;
 class Login extends Common{
     public function _initialize(){
         if (session('user.id')) {
@@ -13,6 +14,7 @@ class Login extends Common{
             $username = input('username');
             $password = input('password');
             $code = input('vercode');
+            dump($username);exit;
             if(!$this->check($code)){
                 return array('code' => 0, 'msg' => '验证码错误');
             }
@@ -32,6 +34,28 @@ class Login extends Common{
                 return array('code'=>1,'msg'=>'登录成功','url' => url('index/index'));
             }
         }else{
+            $sys = F('System');
+            $this->assign('sys',$sys);
+
+            //导航
+            $category = db('category');
+            $thisCat = $category->where('id',input('catId'))->find();
+            $this->assign('title',$thisCat['title']);
+            $this->assign('keywords',$thisCat['keywords']);
+            $this->assign('description',$thisCat['description']);
+            define('DBNAME',strtolower($thisCat['module']));
+            $this->pagesize = $thisCat['pagesize']>0 ? $thisCat['pagesize'] : '';
+            // 获取缓存数据
+            $cate = cache('cate');
+            if(!$cate){
+                $column_one = $category->where(['parentid'=>0,'ismenu'=>1])->order('listorder')->select();
+                $column_two = $category->where(['ismenu'=>1])->order('listorder')->select();;
+                $tree = new Leftnav ();
+                $cate = $tree->index_top($column_one,$column_two);
+                cache('cate', $cate, 3600);
+            }
+            $this->assign('category',$cate);
+
             $plugin = db('plugin')->where(['type'=>'login','status'=>1])->select();
             $this->assign('plugin', $plugin);
             $this->assign('title','会员登录');
